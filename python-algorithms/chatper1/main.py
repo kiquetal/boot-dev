c8f54cfc42451b495478167194bdb8402c9ae0f9
kiquetal <kiquetal@gmail.com>
2024-03-29 12:24:59 -0400

    Adding the main and the test

--- python-algorithms/chatper1/main.py
+++ python-algorithms/chatper1/main.py
@@ -1,13 +1,19 @@
-from typing import Dict, List
-
-
 class Graph:
     def __init__(self):
-        self.graph = Dict[int,List[int]]
+        self.graph = {}
+
     def add_edge(self, u, v):
-       ## check if u resides in graphlib
-       if u in self.graph.keys():
-           l = self.graph[u]
-           l.append(v)
-
-
+        ## check if u resides in graphlib
+        # print(f"Lets add {u} and {v} to the graph")
+        # print(f"Graph: {self.graph}")
+        if u in self.graph.keys():
+            # call add only if is not none
+            self.graph[u].add(v)
+        else:
+            if v is not None:
+                self.graph[u] = {v}
+        if v in self.graph.keys():
+                self.graph[v].add(u)
+        else:
+            if u is not None:
+                self.graph[v] = {u}
