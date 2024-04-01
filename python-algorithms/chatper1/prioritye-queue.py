b0bd6631dc0cfb0a1923b81a3b45cbef7e014ff3
kiquetal <kiquetal@gmail.com>
2024-04-01 09:51:25 -0400

    Adding MinHeap push and bubble_up

--- python-algorithms/chatper1/prioritye-queue.py
+++ python-algorithms/chatper1/prioritye-queue.py
@@ -25,3 +25,36 @@ class PriorityQueue:
 
 
 
+class MinHeap:
+    def push(self, priority, value):
+        index_to_add = -1
+        for i,(p,val) in enumerate(self.elements):
+            if (priority < p):
+                index_to_add = i
+
+        if index_to_add == -1:
+            print("non priority")
+            self.elements.append((priority,value))
+            index_to_add=len(self.elements) -1
+        else:
+            self.elements.insert(index_to_add,(priority,value))
+        self.bubble_up(index_to_add)
+
+    def bubble_up(self, index):
+        if index == 0:
+            return None
+        parent_index = int((index - 1) /2)
+        if self.elements[parent_index][0] > self.elements[index][0]:
+            self.elements[parent_index] = self.elements[index]
+            self.bubble_up(parent_index)
+
+
+    # don't touch below this line
+
+    def __init__(self):
+        self.elements = []
+
+    def peek(self):
+        if len(self.elements) == 0:
+            return None
+        return self.elements[0][1]
