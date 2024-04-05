5454f0191adff60bf1e3e8bffd6a8a396f376e31
kiquetal <kiquetal@gmail.com>
2024-04-05 17:42:59 -0400

    Changing the reference to __hash__

--- python-algorithms/chatper1/tile.py
+++ python-algorithms/chatper1/tile.py
@@ -1,16 +1,20 @@
 import random
 
+
 class Title:
-    def __init__(self,x,y):
+
+    def __init__(self, x, y):
         self.x = x
         self.y = y
 
     def cost(self):
-        random.seed(self.hash())
-        return random.randint(1,25)
+        random.seed(self.__hash__())
+        return random.randint(1, 25)
+
     def __hash__(self):
         return (self.x * 1000) + self.y
 
     def __repr__(self):
         return f"({self.x},{self.y})"
+
 
